fea48e2ae6a22a196e1d4ed79c5cf74d5f62dcee
Jonathan Dey <jon@jrddigital.com>
2024-06-07 16:53:34 +0100

    Initial commit

---
+++ src/Gateway.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Omnipay\TrustPayment;
+
+use Omnipay\Common\AbstractGateway;
+use Omnipay\TrustPayment\Message\PurchaseRequest;
+use Omnipay\TrustPayment\Traits\GatewayParamsTrait;
+
+/**
+ * @method \Omnipay\Common\Message\NotificationInterface acceptNotification(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface authorize(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface completeAuthorize(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface capture(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface completePurchase(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface refund(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface fetchTransaction(array $options = [])
+ * @method \Omnipay\Common\Message\RequestInterface void(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface createCard(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface updateCard(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface deleteCard(array $options = array())
+ */
+class Gateway extends AbstractGateway
+{
+
+    use GatewayParamsTrait;
+
+    /**
+     * Get gateway display name
+     *
+     * This can be used by carts to get the display name for each gateway.
+     * @return string
+     */
+    public function getName()
+    {
+        return 'TrustPayment';
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omnipay\Common\Message\AbstractRequest|\Omnipay\Common\Message\RequestInterface
+     */
+    public function purchase(array $parameters = [])
+    {
+        $request = $this->createRequest(PurchaseRequest::class, $parameters);
+
+        return $request;
+    }
+
+    public function getDefaultParameters()
+    {
+        return [
+            'stprofile' => 'default',
+            'version' => '2',
+        ];
+    }
+}
